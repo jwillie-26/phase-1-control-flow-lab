bd56c806667c678bb877cb155b4e600d5132799c
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2022-11-21 13:41:17 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+if (animal.equals("dog").toLowerCase) {
+    console.log("Bark");
+} else if (animal.equals("cat").toLowerCase) {
+    console.log("Meow");
+} else if (animal.equals("cow").toLowerCase) {
+    console.log("Moo");
+} else if (animal.equals("bird").toLowerCase) {
+    console.log("Tweet");
+} else {
+    console.log("I don't know what that sounds like!");
 }
